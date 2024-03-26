d14e9bfeb7f773f6df0decf0444210cb4ee66a28
Alisrbdni <sarabadaniali@yahoo.com>
2024-03-26 20:31:48 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,14 @@ def icon(emoji: str):
         unsafe_allow_html=True,
     )
 
+hide_streamlit_style = """
+<style>
+    #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 0rem;}
+</style>
 
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # icon("🏎️")
 
 st.subheader("", divider="", anchor=False)
